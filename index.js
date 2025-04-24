80758250d38fadf3667d9676ca59e024d0c84363
root <root@abc.>
2025-04-24 07:21:36 +0000

    L8

---
+++ index.js
@@ -0,0 +1,95 @@
+document.getElementById("form").addEventListener("submit",function(event){
+    event.preventDefault();
+    storeData();
+})
+
+variables=[];
+
+function storeData(){
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let DOB = document.getElementById("DOB").value;
+    let terms = document.getElementById("terms").checked;
+
+    const userData={
+        name,
+        email,
+        password,
+        DOB,
+        terms,
+    }
+     const stringified=JSON.stringify(userData);
+     variables.push(stringified);
+     console.log(variables);
+     localStorage.setItem("user",variables);
+}
+
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    retrieveData();
+    updateEntriesList();
+});
+
+function retrieveData() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let DOB = document.getElementById("DOB").value;
+    let terms = document.getElementById("terms").checked;
+
+    const userData = {
+        name,
+        email,
+        password,
+        DOB,
+        terms
+    };
+
+    
+    let storedData = JSON.parse(localStorage.getItem("userList")) || [];
+    storedData.push(userData);
+    localStorage.setItem("userList", JSON.stringify(storedData));
+
+}
+
+function updateEntriesList() {
+    let storedData = JSON.parse(localStorage.getItem("userList")) || [];
+    let tableBody = document.getElementById("tableBody");
+    tableBody.innerHTML = ""; 
+
+    storedData.forEach((data) => {
+        let tableRow = document.createElement("tr");
+
+
+        let nameCell = document.createElement("td");
+        nameCell.textContent = data.name;
+        tableRow.appendChild(nameCell);
+
+        let emailCell = document.createElement("td");
+        emailCell.textContent = data.email;
+        tableRow.appendChild(emailCell);
+
+        let passwordCell = document.createElement("td");
+        passwordCell.textContent = data.password;
+        tableRow.appendChild(passwordCell);
+
+        let dobCell = document.createElement("td");
+        dobCell.textContent = data.DOB;
+        tableRow.appendChild(dobCell);
+
+        let termsCell = document.createElement("td");
+        termsCell.textContent = data.terms ? "True" : "False";
+        tableRow.appendChild(termsCell);
+
+       
+        tableBody.appendChild(tableRow);
+    });
+}
+
+localStorage.clear();
+
+
+
+
+
